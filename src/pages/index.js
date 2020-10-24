9f32e8984f26e2371a3222cdf4ff8c5a6d41aa58
renataandrade <knattly@gmail.com>
2020-10-24 00:17:30 -0300

    feat: criado página de listagem de personagens

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+const { default: ListCharacter } = require("./Character/ListCharacter");
+
+// Character's Page
+export { default as ListCharacter } from './Character/ListCharacter';
+export { default as DetailCharacter } from './Character/DetailCharacter';
