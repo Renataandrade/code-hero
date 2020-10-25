503e3bf1e3359a6b5204d73ffcc360e8762ea5d1
renataandrade <knattly@gmail.com>
2020-10-25 12:26:05 -0300

    feat: criado página de listagem dos personagens

--- src/pages/Character/ListCharacter.js
+++ src/pages/Character/ListCharacter.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { CardBase } from '../../components';
+
+import '../../assets/styles/pages/_list-character.scss';
+import SearchBase from '../../components/base/SearchBase';
 
 const ListCharacter = () => {
-	return (
-		<h3>List all Characters</h3>
-	)
+  return (
+    <main className="list">
+      <p className="list__title">
+        Busca de personagens
+			</p>
+      
+      {/* Campo de Pesquisa */}
+      <p 
+        className="list__subtitle"
+      >
+        Nome do personagem
+      </p>
+			<SearchBase />
+
+      {/* Listagem de Personagem */}
+      <CardBase />
+      <CardBase />
+    </main>
+  )
 }
 
 export default ListCharacter;
