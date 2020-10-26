72b77674fa7ab1a7700fbafde2730f95851eb1fa
renataandrade <knattly@gmail.com>
2020-10-26 02:25:19 -0300

    feat: adicionao responsividade

--- src/pages/Character/DetailCharacter.js
+++ src/pages/Character/DetailCharacter.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
+import '../../assets/styles/pages/_detail-character.scss';
+import PhotoPNG from '../../assets/images/photo.png';
+import { Carosel } from '../../components';
+
 const DetailCharacter = () => {
 	return (
-		<h3>All details of an specific character</h3>
+		<main>
+      <div className="detail">
+
+        <div className="detail__header">
+          <img className="detail__header-photo" src={PhotoPNG} />
+
+          <h3 className="detail__header-title">
+            Name
+          </h3>
+        </div>
+
+        <div className="detail__body">
+          <p className="detail__description">
+            Rick Jones has been Hulk's best bud since day one, 
+            but now he's more than a friend...he's a teammate! 
+            Transformed by a Gamma energy explosion, A-Bomb's thick, 
+            armored skin is just as strong and powerful as it is blue. 
+            And when he curls into action, 
+            he uses it like a giant bowling ball of destruction!
+          </p>
+        </div>
+      </div>
+
+      <h4 className="detail__title">Comics</h4>
+      <div className="detail__carosel">
+        <Carosel />
+        <Carosel />
+        <Carosel />
+        <Carosel />
+        <Carosel />
+      </div>
+		</main>
 	)
 }
 
--- src/pages/Character/ListCharacter.js
+++ src/pages/Character/ListCharacter.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import { CardBase } from '../../components';
+import { 
+  CardBase, 
+  PaginationTable 
+} from '../../components';
 
 import '../../assets/styles/pages/_list-character.scss';
 import SearchBase from '../../components/base/SearchBase';
+import { Link } from 'react-router-dom';
 
 const ListCharacter = () => {
   return (
     <main className="list">
-      <p className="list__title">
-        Busca de personagens
-			</p>
+      <div className="list__description">
+        <p className="list__description-title">
+          Busca de personagens
+        </p>
+      </div>
       
       {/* Campo de Pesquisa */}
-      <p 
-        className="list__subtitle"
-      >
-        Nome do personagem
-      </p>
 			<SearchBase />
 
       {/* Listagem de Personagem */}
-      <CardBase />
-      <CardBase />
+      <Link to="/character/1">
+        <div className="list__item">
+            <CardBase />
+        </div>
+      </Link>
+      <Link to="/character/1">
+        <div className="list__item">
+            <CardBase />
+        </div>
+      </Link>
+
+      <div className="list__footer">
+        <PaginationTable />
+      </div>
     </main>
   )
 }
